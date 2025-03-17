02dcce1bb53c1b9a7959edb6915a6e0fbcb0b8d4
Pixel Pasta <80410384+PixelPasta@users.noreply.github.com>
2025-03-17 23:21:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 app.set("views", path.join(__dirname, "views"));
+
+app.use("/", async (req, res) => {
+  res.render("index");
+});
 
 app.set("view engine", "ejs");
 // app.use(express.static("public"));
@@ -13,10 +18,6 @@ app.get("/submit", async (req, res) => {
 
 app.get("/reaction", async (req, res) => {
   res.render("reaction", { reaction: req.query.react });
-});
-
-app.use("/", async (req, res) => {
-  res.render("index");
 });
 
 app.listen(PORT, async () => {
